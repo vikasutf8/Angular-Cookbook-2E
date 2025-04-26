1c6bf9f9c39e749ce66927b088f51f7b8c8f6311
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-04-26 11:32:43 +0530

    toasted

--- start/apps/chapter01/ng-cc-signals/src/app/app.component.ts
+++ start/apps/chapter01/ng-cc-signals/src/app/app.component.ts
@@ -1,22 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, viewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '@codewithahsan/ng-cb-ui';
 import { todoStore } from './store/todos.store';
 import { FormsModule } from '@angular/forms';
 import { single } from 'rxjs';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [FormsModule, HeaderComponent, CommonModule, RouterModule, HeaderComponent],
+  imports: [
+    SnackbarComponent,
+    FormsModule, HeaderComponent, CommonModule, RouterModule, HeaderComponent],
 })
 export class AppComponent {
  // their all todo items rendered from todos.store.ts
  store =inject(todoStore);
 
  newTodoTitle =signal(''); //create to getting frontend input 
+snackbar =viewChild.required(SnackbarComponent);
+
+constructor() {
+  console.log('app component');
+  effect(()=>{
+    if(this.store.todos().every((todo)=>todo.completed)){
+      this.snackbar().show();
+    }
+  })
+}
 
  submitNewTodo(){
   this.store.addTodo(this.newTodoTitle()); //on submit frontend todo added in store array
